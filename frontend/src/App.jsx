135f0dd99a19af8ddbcd1ae10abf2fdf42f4da8b
malika7177 <malikadjrv@gmail.com>
2025-02-25 17:01:27 +0500

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from "../src/components/Header/Header.jsx";
+import Navbar from './components/Navbar/Navbar.jsx';
 
 function App() {
 
   return (
     <div>
-      <Header />
+      {/* <Header /> */}
+      <Navbar />
     </div>
   )
 }
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from "react";
+import "./navbar.css";
+import logo from "../../assets/logo.png"
 
 const Navbar = () => {
   return (
     <div>
-      
+        <div className="logo-box">
+          <a href="#">
+            <img src={logo} alt="logo" className="logo" />
+          </a>
+        </div>
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/shop">Shop</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+          <li>
+            <a href="/login">Log in</a>
+          </li>
+          <li>
+            <a href="/signup">Sign up</a>
+          </li>
+        </ul>
+      </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
